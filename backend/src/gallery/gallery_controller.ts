cf49a5ee51386de4a2e68deb146fe6ac71509605
stansful <stansful@gmail.com>
2022-03-16 16:39:28 +0300

    feat: move auth middleware to gallery service, add all request handler
    MODULE-2-PART-3-express

--- backend/src/gallery/gallery_controller.ts
+++ backend/src/gallery/gallery_controller.ts
@@ -2,10 +2,13 @@ import { Router } from 'express';
 import multer from 'multer';
 import { getRequiredPictures, createPicture } from './gallery_service';
 import { config } from '../config/config';
+import { authService } from '../auth/auth_service';
 
 const galleryRouter = Router();
 
 const upload = multer({ dest: config.static.path.uploads });
+
+galleryRouter.use(authService.validateToken);
 
 galleryRouter.route('/gallery').get(getRequiredPictures).post(upload.single('picture'), createPicture);
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,8 +4,8 @@ import { config } from './config/config';
 import { authRouter } from './auth/auth_controller';
 import { galleryRouter } from './gallery/gallery_controller';
 import { errorService } from './error/error_service';
-import { authService } from './auth/auth_service';
 import { logRequestInfo } from './logger/logger_service';
+import { ExceptionService } from './exception/exceptions_service';
 
 const app = express();
 const PORT = config.env.PORT;
@@ -18,10 +18,10 @@ app.use(logRequestInfo);
 app.use('/', express.static('static/pictures'), express.static('static/frontend'));
 
 app.use(authRouter);
-
-app.use(authService.validateToken);
-
 app.use(galleryRouter);
+app.all('*', () => {
+  throw ExceptionService.BadRequest('Path doest not exist');
+});
 
 app.use(errorService.handleError);
 
