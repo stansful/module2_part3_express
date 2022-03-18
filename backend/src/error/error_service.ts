bf706030a6f5fc09a04d332eaa9a441f4d3e09db
stansful <stansful@gmail.com>
2022-03-18 15:34:38 +0300

    feat: remove async because we have noting to await
    MODULE-2-PART-3-express

--- backend/src/error/error_service.ts
+++ backend/src/error/error_service.ts
@@ -3,7 +3,7 @@ import { ExceptionService } from '../exception/exceptions_service';
 import { config } from '../config/config';
 
 class ErrorService {
-  public async handleError(error: Error, req: Request, res: Response, next: NextFunction) {
+  public handleError(error: Error, req: Request, res: Response, next: NextFunction) {
     if (error instanceof ExceptionService) {
       return res.status(error.status).json({ errorMessage: error.message });
     }
